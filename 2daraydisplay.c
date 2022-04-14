1b7a4435208bfad6d2b4ffe70add8bb19ecc3fb9
Satyam Gubrele <95933366+Satyamgubrele@users.noreply.github.com>
2022-04-14 21:53:30 +0530

    Add files via upload

---
+++ 2daraydisplay.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void main()
+{
+int x[3][3],i,j;
+for(i=0;i<3;i++)
+{
+    for(j=0;j<3;j++)
+   { 
+       scanf("%d",&x[i][j]);
+   }
+}
+
+for(i=0;i<3;i++)
+{
+    for(j=0;j<3;j++)
+   { 
+       printf("%d  ",x[i][j]);
+   }
+   printf("\n");
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
